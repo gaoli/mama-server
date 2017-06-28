6539f0395a0876919bc859e08d2f3aa6a4bfcc8f
高力 <3071730@qq.com>
2017-06-28 10:28:26 +0800

    Init

---
+++ config/config.default.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1498615936810_7853';
+
+  // add your config here
+
+  return config;
+};
