515cb95556f78da106be3be5533a716ec5b48078
高力 <3071730@qq.com>
2017-06-30 17:28:06 +0800

    添加 wechat 插件

--- config/plugin.js
+++ config/plugin.js
@@ -1,8 +1,14 @@
 'use strict';
 
+const path = require('path');
+
 module.exports = {
   leancloud: {
     enable: true,
     package: 'egg-leancloud',
+  },
+  wechat: {
+    enable: true,
+    path: path.join(__dirname, '../lib/plugin/egg-wechat'),
   },
 };
