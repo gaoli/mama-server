1ea9dd275d5a702e4995b151dad519bd5a48c624
高力 <3071730@qq.com>
2017-07-04 16:55:53 +0800

    优化食物信息查询服务

--- app/service/food.js
+++ app/service/food.js
@@ -5,11 +5,19 @@ module.exports = app => {
 
   class Food extends app.Service {
     async find(name) {
-      const query = new AV.Query('Food');
-
       let foods = [];
 
-      foods = await query.contains('name', name).find();
+      // 精准匹配
+      const query = new AV.Query('Food');
+
+      foods = await query.equalTo('name', name).find();
+
+      // 模糊匹配
+      if (!foods.length) {
+        const query = new AV.Query('Food');
+
+        foods = await query.contains('name', name).find();
+      }
 
       return foods;
     }
